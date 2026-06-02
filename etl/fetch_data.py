0849e060812420b59ad5c92a4b3bec6e177d57a9
pieter090-droid <pieter090@gmail.com>
2026-06-02 21:02:51 +0200

    Update fetch_data.py

--- etl/fetch_data.py
+++ etl/fetch_data.py
@@ -1,24 +1,16 @@
-import pandas as pd
 import os
-from supabase import create_client
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-# 1. Authenticatie (deze secrets haalt hij veilig uit GitHub)
+# Authenticatie
 os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME')
 os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY')
 api = KaggleApi()
 api.authenticate()
 
-# 2. Download de dataset naar een tijdelijke map
+# Download de dataset
 api.dataset_download_files('saurabhshahane/statsbomb-football-data', path='./data', unzip=True)
 
-# 3. Verbinden met Supabase
-sb = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
-
-# 4. CSV inladen en naar Supabase sturen
-# Let op: zorg dat de bestandsnaam in de zip overeenkomt met wat hieronder staat
-df = pd.read_csv("./data/matches.csv") 
-data = df.to_dict(orient='records')
-sb.table("matches").insert(data).execute()
-
-print("Data succesvol naar de cloud-database gepusht!")
+# KIJK WAAR HET BESTAND IS
+print("Bestanden gevonden in map 'data':")
+for file in os.listdir('./data'):
+    print(file)
